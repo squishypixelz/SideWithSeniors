98cb978adba3529957602de8dacdc507472f773e
Shravan Sithambaram <shravansithambaram@Shravans-MacBook-Air.local>
2026-01-16 20:56:11 -0800

    hi

--- flask-server/flask-server/app.py
+++ flask-server/flask-server/app.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import bcrypt
 from bson.objectid import ObjectId
 
+# hi
 
 baseDirectory = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(baseDirectory, ".env"))
